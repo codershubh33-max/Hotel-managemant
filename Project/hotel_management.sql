5ebb818d87d95f484e6b83f94597b7724da2d24b
Shubham Kumbhar <codershubh33@gmail.com>
2026-07-26 20:29:37 +0530

    add some part on  hotel management project

--- Project/hotel_management.sql
+++ Project/hotel_management.sql
@@ -1,8 +1,5 @@
-
-
 -- Create Database
 CREATE DATABASE HotelManagementSystem;
-
 -- Use Database
 USE HotelManagementSystem;
 
@@ -15,15 +12,11 @@ CREATE TABLE Hotel(
     email VARCHAR(100) UNIQUE
 );
 
-
-
 CREATE TABLE RoomType(
     room_type_id INT PRIMARY KEY AUTO_INCREMENT,
     type_name VARCHAR(50) NOT NULL,
     price_per_night DECIMAL(10,2) NOT NULL
 );
-
-
 
 CREATE TABLE Room(
     room_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -49,19 +42,13 @@ CREATE TABLE Customer(
     city VARCHAR(50)
 );
 
-
-
 CREATE TABLE Booking(
     booking_id INT PRIMARY KEY AUTO_INCREMENT,
-
     customer_id INT,
     room_id INT,
-
     check_in DATE,
     check_out DATE,
-
     total_amount DECIMAL(10,2),
-
     booking_status ENUM('Confirmed','Cancelled','Completed')
     DEFAULT 'Confirmed',
 
@@ -72,49 +59,31 @@ CREATE TABLE Booking(
     REFERENCES Room(room_id)
 );
 
-
 CREATE TABLE Payment(
     payment_id INT PRIMARY KEY AUTO_INCREMENT,
-
     booking_id INT,
-
     payment_date DATE,
-
     payment_method ENUM('Cash','Card','UPI'),
-
     amount DECIMAL(10,2),
-
     payment_status ENUM('Paid','Pending')
     DEFAULT 'Pending',
-
     FOREIGN KEY(booking_id)
     REFERENCES Booking(booking_id)
 );
 
-
-
 CREATE TABLE Staff(
     staff_id INT PRIMARY KEY AUTO_INCREMENT,
-
     hotel_id INT,
-
     first_name VARCHAR(50),
-
     last_name VARCHAR(50),
-
     designation VARCHAR(50),
-
     salary DECIMAL(10,2),
-
     phone VARCHAR(15),
-
     FOREIGN KEY(hotel_id)
     REFERENCES Hotel(hotel_id)
 );
 
-
 -- HOTEL DATA
-
 INSERT INTO Hotel(hotel_name, city, state, phone, email)
 VALUES
 (' shauryawada','Pune','Maharashtra','9876543210','shauya@hotel.com'),
@@ -122,7 +91,6 @@ VALUES
 ('akshay','sinhgad','Maharashtra','9876543212','akshay@hotel.com');
 
 -- ROOM TYPES
-
 INSERT INTO RoomType(type_name, price_per_night)
 VALUES
 ('Single',1500),
@@ -130,10 +98,7 @@ VALUES
 ('triple',4500),
 ('bighall',3000);
 
-
-
 -- ROOMS
-
 INSERT INTO Room(hotel_id, room_number, room_type_id, status)
 VALUES
 (1,'101',1,'Available'),
@@ -150,7 +115,6 @@ VALUES
 (3,'303',4,'Available');
 
 -- CUSTOMERS
-
 INSERT INTO Customer(first_name,last_name,gender,phone,email,city)
 VALUES
 ('Rahul','Sharma','Male','9876500001','rahul@gmail.com','Pune'),
@@ -159,10 +123,7 @@ VALUES
 ('Sneha','Kulkarni','Female','9876500004','sneha@gmail.com','Nashik'),
 ('Rohan','Patel','Male','9876500005','rohan@gmail.com','Surat');
 
-------------------------------------------------
-
 -- BOOKINGS
-
 INSERT INTO Booking(customer_id,room_id,check_in,check_out,total_amount,booking_status)
 VALUES
 (1,2,'2026-07-28','2026-07-30',5000,'Confirmed'),
@@ -171,10 +132,7 @@ VALUES
 (4,3,'2026-08-01','2026-08-04',13500,'Confirmed'),
 (5,10,'2026-08-05','2026-08-07',14000,'Cancelled');
 
-------------------------------------------------
-
 -- PAYMENTS
-
 INSERT INTO Payment(booking_id,payment_date,payment_method,amount,payment_status)
 VALUES
 (1,'2026-07-28','UPI',5000,'Paid'),
@@ -184,7 +142,6 @@ VALUES
 (5,'2026-08-05','Card',14000,'Pending');
 
 -- STAFF
-
 INSERT INTO Staff(hotel_id,first_name,last_name,designation,salary,phone)
 VALUES
 (1,'Suresh','Patil','Manager',55000,'9999000001'),
@@ -192,19 +149,96 @@ VALUES
 (2,'Ajay','More','Manager',60000,'9999000003'),
 (2,'Pooja','Joshi','Receptionist',32000,'9999000004'),
 (3,'Vikas','Kale','Housekeeping',25000,'9999000005');
-
-
 --  practice
--- SELECT * FROM Hotel;
+SELECT * FROM Hotel;
+SELECT * FROM RoomType;
+SELECT * FROM Room;
+SELECT * FROM Customer;
+SELECT * FROM Booking;
+SELECT * FROM Payment;
+SELECT * FROM Staff;
 
--- SELECT * FROM RoomType;
+-- Display all hotels
+SELECT * FROM Hotel;
 
--- SELECT * FROM Room;
+-- Display all available rooms
+SELECT * FROM Room
+WHERE status = 'Available';
 
--- SELECT * FROM Customer;
+-- Display all booked rooms
+SELECT * FROM Room
+WHERE status = 'Booked';
 
--- SELECT * FROM Booking;
+-- Display all customers
+SELECT * FROM Customer;
 
--- SELECT * FROM Payment;
+-- 5. Display bookings with customer names
+SELECT
+    b.booking_id,
+    c.first_name,
+    c.last_name,
+    b.check_in,
+    b.check_out,
+    b.total_amount,
+    b.booking_status
+FROM Booking b
+INNER JOIN Customer c
+ON b.customer_id = c.customer_id;
 
--- SELECT * FROM Staff;
+-- 6. Display room details with room type
+SELECT
+    r.room_number,
+    rt.type_name,
+    rt.price_per_night,
+    r.status
+FROM Room r
+INNER JOIN RoomType rt
+ON r.room_type_id = rt.room_type_id;
+
+-- 7. Display hotel name with room number
+SELECT
+    h.hotel_name,
+    r.room_number,
+    r.status
+FROM Hotel h
+INNER JOIN Room r
+ON h.hotel_id = r.hotel_id;
+
+SELECT
+c.first_name,
+b.booking_id
+FROM Customer c
+JOIN Booking b
+ON c.customer_id = b.customer_id;
+
+--  Total number of customers
+SELECT COUNT(*) AS Total_Customers
+FROM Customer;
+
+--  Average room price
+SELECT AVG(price_per_night) AS Average_Room_Price
+FROM RoomType;
+
+-- Highest room price
+SELECT MAX(price_per_night) AS Highest_Price
+FROM RoomType;
+
+-- Pending payments
+SELECT *
+FROM Payment
+WHERE payment_status='Pending';
+
+-- Customers from Pune
+SELECT *
+FROM Customer
+WHERE city='Pune';
+
+-- Sort customers alphabetically
+SELECT *
+FROM Customer
+ORDER BY first_name;
+
+SELECT *
+FROM Payment
+ORDER BY amount DESC
+LIMIT 3;
